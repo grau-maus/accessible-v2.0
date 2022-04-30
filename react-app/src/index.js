fe8862a7a9a13abfc3f41ce9efc4d8f5ac8b4a09
grau-maus <joshtupas@gmail.com>
2022-04-30 16:18:49 -0600

    change deploy branch to 'deploy'. add chakra-ui framework.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import App from './App';
 import configureStore from './store';
@@ -10,8 +11,10 @@ const store = configureStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ChakraProvider>
         <App />
-      </Provider>
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
